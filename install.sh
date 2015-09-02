cf5e77179253756f50b54ba73caade05bf5d5eec
Matthew Morrissey <mattamizer@gmail.com>
2015-09-02 10:07:44 -0400

    We have to install oh-my-zsh up front for everything else to work.

--- install.sh
+++ install.sh
@@ -42,6 +42,3 @@ git config --global web.browser open
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
