f88c7bbb1a351061c71764eea2cdf2f3ed3a5e0c
Matthew Morrissey <mattamizer@gmail.com>
2016-09-12 14:21:15 -0400

    Temporarily remove the automated zprezto install

--- prezto.sh
+++ prezto.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Clone prezto
-git clone --recursive https://github.com/mattamizer/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-# Install
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
-# Set zsh as the default shell
-chsh -s /bin/zsh
+echo "Go to https://github.com/mattamizer/prezto and follow the instructions there"
