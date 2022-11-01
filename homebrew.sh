c95abb12055b1861020fab004567eec0a43b506f
Matthew Morrissey <mattamizer@pm.me>
2022-11-01 15:34:46 -0400

    Use `brew bundle` to install brew taps

--- homebrew.sh
+++ homebrew.sh
@@ -1,39 +1,11 @@
 #!/bin/bash -e
 
-which -s brew && brew update
 which -s brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which -s brew && brew update
 
 if ! brew doctor; then
   echo "\`brew doctor\` failed. Please resolve issues before continuing."
   exit 1
 fi
 
-brew tap neovim/homebrew-neovim
-brew tap thoughtbot/formulae
-
-formulae=(
-  ag
-  chruby
-  cloc
-  ctags
-  flake8
-  git
-  heroku-toolbelt
-  hub
-  jq
-  neovim
-  pick
-  pyenv
-  reattach-to-user-namespace
-  ruby-install
-  slackcat
-  tmate
-  tree
-  watch
-  wemux
-  wget
-)
-
-for formula in "${formulae[@]}"; do
-  brew install $formula || brew upgrade $formula
-done
+brew bundle
