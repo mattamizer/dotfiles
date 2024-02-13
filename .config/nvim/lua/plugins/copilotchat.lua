561a64aad4749f053c97296da58f1fbe9de38b6d
Matthew Morrissey <mattamizer@pm.me>
2024-02-13 14:42:31 -0500

    Switch to new CopilotChat fork

--- .config/nvim/lua/plugins/copilotchat.lua
+++ .config/nvim/lua/plugins/copilotchat.lua
@@ -1,35 +1,42 @@
-local prompts = {
-	-- Code related prompts
-	Explain = "Please explain how the following code works.",
-	Review = "Please review the following code and provide suggestions for improvement.",
-	Tests = "Please explain how the selected code works, then generate unit tests for it.",
-	Refactor = "Please refactor the following code to improve its clarity and readability.",
-	-- Text related prompts
-	Summarize = "Please summarize the following text.",
-	Spelling = "Please correct any grammar and spelling errors in the following text.",
-	Wording = "Please improve the grammar and wording of the following text.",
-	Concise = "Please rewrite the following text to make it more concise.",
-}
-
 return {
-	-- Import the copilot plugin
-	{ import = "lazyvim.plugins.extras.coding.copilot" },
-	{
-		"jellydn/CopilotChat.nvim",
-		-- Will be merged to main branch soon
-		dependencies = { "github/copilot.vim" },
-		branch = "canary",
-		opts = {
-			mode = "split",
-			show_help = "yes",
-			prompts = prompts,
+	"CopilotC-Nvim/CopilotChat.nvim",
+	event = "VeryLazy",
+	opts = {
+		prompts = {
+			Explain = "Explain how it works.",
+			Review = "Review the following code and provide concise suggestions.",
+			Tests = "Briefly explain how the selected code works, then generate unit tests.",
+			Refactor = "Refactor the code to improve clarity and readability.",
 		},
-		build = function()
-			vim.defer_fn(function()
-				vim.cmd("UpdateRemotePlugins")
-				vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
-			end, 3000)
-		end,
-		event = "VeryLazy",
+	},
+	build = function()
+		vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+	end,
+	config = function()
+		local present, wk = pcall(require, "which-key")
+		if not present then
+			return
+		end
+
+		wk.register({
+			c = {
+				c = {
+					name = "Copilot Chat",
+				},
+			},
+		}, {
+			mode = "n",
+			prefix = "<leader>",
+			silent = true,
+			noremap = true,
+			nowait = false,
+		})
+	end,
+	keys = {
+		{ "<leader>ccc", ":CopilotChat ", desc = "CopilotChat - Prompt" },
+		{ "<leader>cce", ":CopilotChatExplain ", desc = "CopilotChat - Explain code" },
+		{ "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+		{ "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
+		{ "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
 	},
 }
