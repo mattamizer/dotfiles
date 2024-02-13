d3bc49895d936d4e8f0c965a2fe5f29fadd53776
Matthew Morrissey <mattamizer@pm.me>
2024-02-13 11:48:44 -0500

    Cleanup overlap with my config v lazy config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,56 @@
 -- Catppuccin Theme
 return {
-  -- https://github.com/catppuccin/nvim
-  "catppuccin/nvim",
-  name = "catppuccin", -- name is needed otherwise plugin shows up as "nvim" due to github URI
-  lazy = false,        -- We want the colorscheme to load immediately when starting Neovim
-  priority = 1000,     -- Load the colorscheme before other non-lazy-loaded plugins
-  opts = {
-    --   -- Replace this with your scheme-specific settings or remove to use the defaults
-    -- transparent = true,
-    flavour = "macchiato", -- "latte, frappe, macchiato, mocha"
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-      dashboard = true,
-      markdown = true,
-      dap = true,
-      dap_ui = true,
-      telescope = { enabled = true },
-      rainbow_delimiters = true
-    },
-  },
-  config = function(_, opts)
-    require("catppuccin").setup(opts) -- Replace this with your favorite colorscheme
-    vim.cmd("colorscheme catppuccin") -- Replace this with your favorite colorscheme
-  end
+	-- https://github.com/catppuccin/nvim
+	"catppuccin/nvim",
+	name = "catppuccin", -- name is needed otherwise plugin shows up as "nvim" due to github URI
+	lazy = false, -- We want the colorscheme to load immediately when starting Neovim
+	priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
+	opts = {
+		--   -- Replace this with your scheme-specific settings or remove to use the defaults
+		-- transparent = true,
+		flavour = "macchiato", -- "latte, frappe, macchiato, mocha"
+		integrations = {
+			aerial = true,
+			alpha = true,
+			cmp = true,
+			dap = true,
+			dap_ui = true,
+			dashboard = true,
+			flash = true,
+			gitsigns = true,
+			headlines = true,
+			illuminate = true,
+			indent_blankline = { enabled = true },
+			leap = true,
+			lsp_trouble = true,
+			mason = true,
+			markdown = true,
+			mini = true,
+			native_lsp = {
+				enabled = true,
+				underlines = {
+					errors = { "undercurl" },
+					hints = { "undercurl" },
+					warnings = { "undercurl" },
+					information = { "undercurl" },
+				},
+			},
+			navic = { enabled = true, custom_bg = "lualine" },
+			neotest = true,
+			neotree = true,
+			noice = true,
+			notify = true,
+			semantic_tokens = true,
+			telescope = true,
+			treesitter = true,
+			treesitter_context = true,
+			which_key = true,
+			nvimtree = true,
+			rainbow_delimiters = true,
+		},
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts) -- Replace this with your favorite colorscheme
+		vim.cmd("colorscheme catppuccin") -- Replace this with your favorite colorscheme
+	end,
 }
