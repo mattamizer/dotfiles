2c106ef0e7c9c78b7c69b69312d59c2604869a59
Matthew Morrissey <matthew.morrissey@protonmail.com>
2024-11-13 12:38:27 -0500

    Update LazyVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,7 +25,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.dap.nlua" },
 		{ import = "lazyvim.plugins.extras.formatting.black" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
-		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
