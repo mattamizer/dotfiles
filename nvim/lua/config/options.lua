20c4746ff54e09fa5be6242661d80f8a2f650577
Matthew Morrissey <matthew.morrissey@protonmail.com>
2025-05-21 15:29:46 -0400

    Lazy Updates; use ruff over black

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -15,5 +15,10 @@ vim.g.python3_host_prog = "/Users/mmorrissey/.pyenv/versions/neovim/bin/python"
 -- Enable this option to avoid conflicts with Prettier.
 vim.g.lazyvim_prettier_needs_config = false
 
+-- Use Ruff
+vim.g.lazyvim_python_ruff = "ruff"
+-- Use Pyright
+vim.g.lazyvim_python_lsp = "pyright"
+
 -- Please stop with the AI nonsense
 vim.g.ai_cmp = false
