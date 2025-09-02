95193bbdf661934aab28982fefec4f2ecd5c0aea
Matthew Morrissey <matthew.morrissey@protonmail.com>
2025-09-02 15:24:03 -0400

    Add working configuration for snyk-ls

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,40 @@
 return {
 	"neovim/nvim-lspconfig",
+	event = "LazyFile",
+	dependencies = {
+		"mason.nvim",
+		{ "mason-org/mason-lspconfig.nvim", config = function() end },
+	},
 	opts = {
 		servers = {
 			eslint = {},
+			snyk_ls = {
+				mason = false,
+				cmd = { "/opt/homebrew/bin/snyk", "language-server" },
+				root_markers = { ".git", ".snyk" },
+				filetypes = {
+					"go",
+					"gomod",
+					"javascript",
+					"typescript",
+					"json",
+					"python",
+					"requirements",
+					"helm",
+					"yaml",
+					"terraform",
+					"terraform-vars",
+				},
+				settings = {},
+				-- Configuration from https://github.com/snyk/snyk-ls#configuration-1
+				init_options = {
+					activateSnykCode = "true",
+					authenticationMethod = "oauth",
+					automaticAuthentication = "true",
+					cliPath = "/opt/homebrew/bin/snyk",
+					organization = "relay-network",
+				},
+			},
 		},
 		setup = {
 			eslint = function()
