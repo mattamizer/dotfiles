15eef363512c7b8ca9344b1f2d6b46b16cc9c748
Matthew Morrissey <matthew.morrissey@protonmail.com>
2026-02-02 16:37:21 -0500

    Remove smart splits and associated configuration
    We're not really using this as part of our workflow anymore.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -102,14 +102,4 @@ wpr.apply_to_config(config, {
 	},
 })
 
--- Smart Splits
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-smart_splits.apply_to_config(config, {
-	direction_keys = { "h", "j", "k", "l" },
-	modifiers = {
-		move = "CTRL",
-		resize = "ALT",
-	},
-})
-
 return config
