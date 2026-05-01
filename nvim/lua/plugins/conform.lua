6f62a11682d4d225530a69d6b8055ac15576f761
Matthew Morrissey <matthew.morrissey@protonmail.com>
2026-05-01 08:31:59 -0400

    chore: add default prettier config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,5 +1,11 @@
 return {
 	"stevearc/conform.nvim",
+	dependencies = {
+		{
+			"prettier/vim-prettier",
+			build = { "yarn install --frozen-lockfile --production" },
+		},
+	},
 	opts = {
 		formatters_by_ft = {
 			tf = { "tfmt" },
