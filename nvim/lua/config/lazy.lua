418419b484953b6f28c154731eb2f52b2cc85a84
Matthew Morrissey <matthew.morrissey@protonmail.com>
2026-06-22 08:56:33 -0400

    chore: LazyVim updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -44,6 +44,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.terraform" },
 		{ import = "lazyvim.plugins.extras.lang.toml" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.typescript.tsgo" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.test.core" },
